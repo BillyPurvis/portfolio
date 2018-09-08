df3c36d07bfc6a214add9a9fde21d6638534caec
Glen Sargent <glensarg@hotmail.co.uk>
2018-09-08 21:55:31 +0100

    fini

---
+++ components/Brand.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="branding">
+    <h1>Glen Sargent</h1>
+    <ul>
+      <a href="mailto:glen@muttnik.io"><li>Email</li></a>
+      <a href="https://twitter.com/glensarge"><li>Twitter</li></a>
+      <a href="https://linkedin.com/in/glensargent/"><li>LinkedIn</li></a>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.branding {
+  font-size: 12px;
+  margin: 50px 0;
+  h1 {
+    text-transform: uppercase;
+    font-size: inherit;
+    font-weight: bold;
+  }
+  ul {
+    padding: 0;
+    margin: 0;
+    li {
+      display: inline-block;
+      margin-right: 10px;
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="wrapper">
+    <Brand/>
     <nuxt/>
     <my-footer/>
   </div>
@@ -7,46 +8,19 @@
 
 <script>
 import MyFooter from '~/components/Footer.vue'
+import Brand from '~/components/Brand.vue'
 
 export default {
   components: {
-    MyFooter
+    MyFooter,
+    Brand
   }
 }
 </script>
 
-<style>
-.container
-{
-  margin: 0;
-  width: 100%;
-  padding: 100px 0;
-  text-align: center;
-}
-
-.button, .button:visited
-{
-  display: inline-block;
-  color: black;
-  letter-spacing: 1px;
-  background-color: #fff;
-  border: 2px solid #000;
-  text-decoration: none;
-  text-transform: uppercase;
-  padding: 15px 45px;
-}
-
-.button:hover, .button:focus
-{
-  color: #fff;
-  background-color: #000;
-}
-
-.title
-{
-  color: #000;
-  font-weight: 300;
-  font-size: 2.5em;
-  margin: 0;
-}
+<style lang="scss">
+.wrapper {
+  max-width: 990px;
+  margin: 0 auto;
+} 
 </style>
